e0694abae4a270a8c20d295c45d112017ddca5da
ajioe1111 <ajioe1111@gmail.com>
2024-03-02 13:47:04 +0300

    Обновлён gitignore и третий пример

--- Ex/3/Program.cs
+++ Ex/3/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите имя пользователя: ");
+string usernames = Console.ReadLine();
+
+// Преобразование ввода пользователя и строки для сравнения в нижний регистр
+if (usernames.ToLower() == "masha")
+{
+    Console.WriteLine("Это Маша!");
+}
+else
+{
+    Console.WriteLine("Привет ");
+    Console.WriteLine(usernames);
+}
--- Ex/3/obj/Debug/net8.0/3.AssemblyInfo.cs
+++ Ex/3/obj/Debug/net8.0/3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f99ec3953bb8728a91fcb488eed060d572847d")]
 [assembly: System.Reflection.AssemblyProductAttribute("3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
