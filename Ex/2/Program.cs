660daa5d6e021326c3eda6eec50b776eab3961be
ajioe1111 <ajioe1111@gmail.com>
2024-03-02 13:37:50 +0300

    Добавлена папка Ex

---
+++ Ex/2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя: ");
+string usernames = Console.ReadLine();
+
+if (usernames == "Masha")
+{
+    Console.WriteLine("Это Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(usernames);
+}
