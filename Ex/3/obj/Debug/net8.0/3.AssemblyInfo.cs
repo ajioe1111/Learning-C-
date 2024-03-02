a36bf0a0a258e7744e4d85d9c0d1462199bfa1a1
ajioe1111 <ajioe1111@gmail.com>
2024-03-02 13:52:13 +0300

    add Sem1

--- Ex/3/obj/Debug/net8.0/3.AssemblyInfo.cs
+++ Ex/3/obj/Debug/net8.0/3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0694abae4a270a8c20d295c45d112017ddca5da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03361201bc849e581a0f207eac6f9533e6ba485f")]
 [assembly: System.Reflection.AssemblyProductAttribute("3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
