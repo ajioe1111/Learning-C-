cc6a8dacf455d532e7108dd77452bf0e62e7d552
ajioe1111 <ajioe1111@gmail.com>
2024-03-02 14:40:11 +0300

    Задача 3

--- Ex/1/obj/Debug/net8.0/1.AssemblyInfo.cs
+++ Ex/1/obj/Debug/net8.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+383f1a4eb9df88edd5814a745b8ef5cda30d6586")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4e07a89929d769ecd6a1f07e6c297ed0b6bfb4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Sem1/task3/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача №3
+// Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// Например:
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Введите номер дня недели: ");
+string input = Console.ReadLine();
+
+// Попытка преобразовать введенную строку в число
+bool isSuccess = int.TryParse(input, out int dayNumber);
+
+if (isSuccess && dayNumber >= 1 && dayNumber <= 7)
+{
+    switch (dayNumber)
+    {
+        case 1:
+            Console.WriteLine("Понедельник");
+            break;
+        case 2:
+            Console.WriteLine("Вторник");
+            break;
+        case 3:
+            Console.WriteLine("Среда");
+            break;
+        case 4:
+            Console.WriteLine("Четверг");
+            break;
+        case 5:
+            Console.WriteLine("Пятница");
+            break;
+        case 6:
+            Console.WriteLine("Суббота");
+            break;
+        case 7:
+            Console.WriteLine("Воскресенье");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Некорректный ввод. Введите число от 1 до 7.");
+}
