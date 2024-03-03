0d66ed7543cb1b8e200bb19f4e1a56a7971d11bc
ajioe1111 <ajioe1111@gmail.com>
2024-03-03 16:31:24 +0300

    Задача 16

---
+++ Sem2/task16/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача №16
+// Напишите программу, которая принимает на вход
+// два числа и проверяет, является ли одно число
+// квадратом другого.
+
+// Console.WriteLine("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (firstNumber * firstNumber == secondNumber || secondNumber * secondNumber == firstNumber)
+// {
+//     Console.WriteLine("Да, одно число является квадратом другого.");
+// }
+// else
+// {
+//     Console.WriteLine("Нет, ни одно из чисел не является квадратом другого.");
+// }
+
+{
+    Console.WriteLine("Введите первое число: ");
+    int firstNumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число: ");
+    int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    if (IsSquareOf(firstNumber, secondNumber))
+    {
+        Console.WriteLine("Да, одно число является квадратом другого.");
+    }
+    else
+    {
+        Console.WriteLine("Нет, ни одно из чисел не является квадратом другого.");
+    }
+}
+
+bool IsSquareOf(int a, int b)
+{
+    return a * a == b || b * b == a;
+}
