ee76b5e13c58b040db0904afb465a24c13cfe749
ajioe1111 <ajioe1111@gmail.com>
2024-03-03 21:11:43 +0300

    Задача 13

---
+++ Sem2/task10/Program.cs
@@ -0,0 +1,21 @@
+﻿// №10 Напишите программу, которая принимает на вход трёхзначное число и на выходе
+// показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    string numberAsString = number.ToString();
+    char secondDigit = numberAsString[1];
+
+    Console.WriteLine($"Вторая цифра числа: {secondDigit}");
+}
+else
+{
+    Console.WriteLine("Введенное число не является трехзначным.");
+}
---
+++ Sem2/task13/Program.cs
@@ -0,0 +1,32 @@
+﻿// №13 Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// # Сделать вариант для числа длиной до 10 цифр не используя char или string
+
+Console.WriteLine("Введите число (до 10 чисел): ");
+string number = Console.ReadLine();
+
+// Проверяем на Null
+if (number != null)
+{
+    int parsNumber = int.Parse(number);
+    if (parsNumber >= 100)
+    {
+        while (parsNumber >= 1000)
+        {
+            parsNumber /= 10;
+        }
+        int result = (int)parsNumber % 10;
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine("Третий цифры нету");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка: null");
+}
