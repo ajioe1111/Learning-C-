18b2e1950d3fe02307cf184aea3412bf94a93cf6
ajioe1111 <ajioe1111@gmail.com>
2024-03-03 15:21:43 +0300

    Задача 9

---
+++ Sem2/task9/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача №9
+// Напишите программу, которая выводит случайное
+// число из отрезка [10, 99] и показывает наибольшую
+// цифру числа.
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+System.Random numSintezator = new System.Random();
+
+//Вариант 1
+int rndNumber = numSintezator.Next(10, 100);
+Console.WriteLine(rndNumber);
+int fn = rndNumber/10;
+int sn = rndNumber%10;
+
+if(fn>sn)
+{
+    Console.WriteLine(fn);
+}
+else
+{
+    Console.WriteLine(sn);
+}
